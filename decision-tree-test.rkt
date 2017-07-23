ced580d8b04c626b5dd937807baf331f5b23b3b1
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2017-07-24 01:15:01 +0200

    initial commit

---
+++ decision-tree-test.rkt
@@ -0,0 +1,246 @@
+#lang racket
+
+
+(require rackunit)
+(require "decision-tree.rkt")
+
+(define (vector-set v i o)
+  (vector->immutable-vector
+   (for/vector ([j (in-range (vector-length v))])
+     (if (= i j)
+         o
+         (vector-ref v j)))))
+
+(define TEST-DATA #(#(2.771244718 1.784783929 0)
+                    #(1.728571309 1.169761413 0)
+                    #(3.678319846 2.81281357 0)
+                    #(3.961043357 2.61995032 0)
+                    #(2.999208922 2.209014212 0)
+                    #(7.497545867 3.162953546 1)
+                    #(9.00220326 3.339047188 1)
+                    #(7.444542326 0.476683375 1)
+                    #(10.12493903 3.234550982 1)
+                    #(6.642287351 3.319983761 1)))
+(define PRECISION (expt 10 -9))
+
+(test-case "data-empty? test case"
+  (check-true (data-empty? #())
+              "data-empty? is not correct"))
+
+(test-case "data-first test case"
+  (check-equal? (data-first #(#(0 0 0)
+                              #(1 1 1)))
+                #(0 0 0)
+                "data-first is not correct"))
+
+(test-case "data-rest test case"
+  (check-equal? (data-rest #(#(0 0 0)
+                             #(1 1 1)))
+                #(#(1 1 1))
+                "data-rest is not correct")
+  (check-equal? (data-rest #(#(0 0 0)
+                             #(1 1 1)
+                             #(2 2 2)
+                             #(3 3 3)))
+                #(#(1 1 1)
+                  #(2 2 2)
+                  #(3 3 3))
+                "data-rest is not correct"))
+
+(test-case "data-length test case"
+  (check-equal? (data-length #(#(0)
+                               #(1)
+                               #(2)))
+                3
+                "data-length is not correct")
+  (check-equal? (data-length #(#(0)
+                               #(1)
+                               #(2 3)
+                               #(3 4 5)))
+                4
+                "data-length is not correct"))
+
+(test-case "data-point-length test case"
+  (check-equal? (data-point-length #(0.1 0.2 0.3245 123.213432 'a))
+                5
+                "data-point-length is not correct"))
+
+(test-case "data-filter test-case"
+  (check-equal? (data-filter (lambda (data-point)
+                               (> (data-point-get-col data-point 1) 3))
+                             TEST-DATA)
+                #(#(7.497545867 3.162953546 1)
+                  #(9.00220326 3.339047188 1)
+                  #(10.12493903 3.234550982 1)
+                  #(6.642287351 3.319983761 1)))
+  (check-equal? (data-filter (lambda (data-point)
+                               (< (data-point-get-col data-point 0) 2))
+                             TEST-DATA)
+                #(#(1.728571309 1.169761413 0))))
+
+
+(define (vectors-approximately-equal? v1 v2 epsilon)
+  (define (approximately-equal? value1 value2 epsilon)
+    (<= (abs (- (abs value1) (abs value2))) epsilon))
+
+  (define (check-elements v1 v2 epsilon)
+    (cond [(and (= (vector-length v1) 0) (= (vector-length v2) 0)) true]
+          [(approximately-equal? (vector-ref (vector-ref (vector-take v1 1) 0) 0)
+                                 (vector-ref (vector-ref (vector-take v2 1) 0) 0)
+                                 epsilon)
+           (check-elements (vector-take-right v1 (- (vector-length v1) 1))
+                           (vector-take-right v2 (- (vector-length v2) 1))
+                           epsilon)]
+          [else false]))
+
+  (if (= (vector-length v1) (vector-length v2))
+      (check-elements v1 v2 epsilon)
+      false))
+
+(test-case "data-map test case"
+  (check-equal?
+   (vectors-approximately-equal?
+    (data-map (lambda (data-point)
+                (let ([a-copy (vector-copy data-point)])
+                  (vector-set a-copy
+                              0
+                              (+ (vector-ref data-point 0) 1.0))))
+              TEST-DATA)
+    #(#(3.771244718 1.784783929 0)
+      #(2.728571309 1.169761413 0)
+      #(4.678319846 2.81281357 0)
+      #(4.961043357 2.61995032 0)
+      #(3.999208922 2.209014212 0)
+      #(8.497545867 3.162953546 1)
+      #(10.00220326 3.339047188 1)
+      #(8.444542326 0.476683375 1)
+      #(11.12493903 3.234550982 1)
+      #(7.642287351 3.319983761 1))
+    PRECISION)
+   true))
+
+(test-case "data-get-col test case"
+  (check-equal? (data-get-col #(#(123 43 1)
+                                #(2132435 23 2)
+                                #(1244 769 3))
+                              2)
+                #(1 2 3)))
+
+(test-case "data-get-row test case"
+  (check-equal? (data-get-row #(#(123 43 1)
+                                #(2132435 23 2)
+                                #(1244 769 3))
+                              2)
+                #(1244 769 3)))
+
+(test-case "class-equals? test case"
+  (check-true (class-equals? 1 1))
+  (check-true (class-equals? 0 0))
+  (check-true (class-equals? 2 2))
+  (check-false (class-equals? 1 0))
+  (check-false (class-equals? 2 0))
+  (check-false (class-equals? 1 2)))
+
+(test-case "data-point-get-col"
+  (check-equal? (data-point-get-col #(0 32 478 282 1) 1)
+                32)
+  (check-equal? (data-point-get-col #(0 32 478 282 1) 0)
+                0))
+
+(test-case "gini-index test case"
+  (check-equal? (gini-index (list
+                             #(#(1.1 2.2 3.3 4.4 0)
+                               #(1.1 2.2 3.3 4.4 0))
+                             #(#(1.1 2.2 3.3 4.4 1)
+                               #(1.1 2.2 3.3 4.4 1)))
+                            #(0 1)
+                            4)
+                0.0
+                "gini index of perfect split is not 0.0")
+  (check-equal? (gini-index (list
+                             #(#(1.1 2.2 3.3 4.4 0)
+                               #(1.1 2.2 3.3 4.4 1))
+                             #(#(1.1 2.2 3.3 4.4 0)
+                               #(1.1 2.2 3.3 4.4 1)))
+                            #(0 1)
+                            4)
+                1.0
+                "gini index of worst split is not 1.0")
+  (check-= (gini-index (list
+                        #(#(1.1 2.2 3.3 4.4 0)
+                          #(1.1 2.2 3.3 4.4 1)
+                          #(1.1 2.2 3.3 4.4 1))
+                        #(#(1.1 2.2 3.3 4.4 0)
+                          #(1.1 2.2 3.3 4.4 0)
+                          #(1.1 2.2 3.3 4.4 1)))
+                       #(0 1)
+                       4)
+           0.888888888
+           PRECISION
+           (string-append "gini index of split is not "
+                          (number->string 0.888888888))))
+
+(test-case
+    "split-data test case"
+  (check-equal? (split-data #(#(1.0 1.0 1.0 1.0 0)
+                              #(1.2 1.0 1.0 1.0 0)
+                              #(1.4 1.0 1.0 1.0 0)
+                              #(1.6 1.0 1.0 1.0 0)
+                              #(1.8 1.0 1.0 1.0 0)
+                              #(2.0 1.0 1.0 1.0 0))
+                            0
+                            1.5)
+                (list #(#(1.0 1.0 1.0 1.0 0)
+                        #(1.2 1.0 1.0 1.0 0)
+                        #(1.4 1.0 1.0 1.0 0))
+                      #(#(1.6 1.0 1.0 1.0 0)
+                        #(1.8 1.0 1.0 1.0 0)
+                        #(2.0 1.0 1.0 1.0 0)))
+                "split-data does not split correctly")
+  (check-equal? (split-data #(#(1.0 1.0 1.0 1.0 0)
+                              #(1.2 4.0 1.0 1.0 0)
+                              #(1.4 1.0 1.0 1.0 0)
+                              #(1.6 3.0 1.0 1.0 0)
+                              #(1.8 1.0 1.0 1.0 0)
+                              #(2.0 2.0 1.0 1.0 0))
+                            1
+                            2.5)
+                (list #(#(1.0 1.0 1.0 1.0 0)
+                        #(1.4 1.0 1.0 1.0 0)
+                        #(1.8 1.0 1.0 1.0 0)
+                        #(2.0 2.0 1.0 1.0 0))
+                      #(#(1.2 4.0 1.0 1.0 0)
+                        #(1.6 3.0 1.0 1.0 0)))
+                "split-data does not split correctly"))
+
+(test-case "get-best-split test case"
+  (let ([test-data #(#(2.771244718 1.784783929 0)
+                     #(1.728571309 1.169761413 0)
+                     #(3.678319846 2.81281357 0)
+                     #(3.961043357 2.61995032 0)
+                     #(2.999208922 2.209014212 0)
+                     #(7.497545867 3.162953546 1)
+                     #(9.00220326 3.339047188 1)
+                     #(7.444542326 0.476683375 1)
+                     #(10.12493903 3.234550982 1)
+                     #(6.642287351 3.319983761 1))]
+        [feature-columns-indices (list 0 1)]
+        [label-column-index 2])
+    (check-equal? (get-best-split test-data
+                                  gini-index
+                                  feature-columns-indices
+                                  label-column-index)
+                  (hash 'index 0
+                        'value 6.642287351
+                        'subsets (list #(#(2.771244718 1.784783929 0)
+                                         #(1.728571309 1.169761413 0)
+                                         #(3.678319846 2.81281357 0)
+                                         #(3.961043357 2.61995032 0)
+                                         #(2.999208922 2.209014212 0))
+                                       #(#(7.497545867 3.162953546 1)
+                                         #(9.00220326 3.339047188 1)
+                                         #(7.444542326 0.476683375 1)
+                                         #(10.12493903 3.234550982 1)
+                                         #(6.642287351 3.319983761 1)))
+                        'cost 0.0)
+                  "get-best-split does not give the best split")))
---
+++ decision-tree.rkt
@@ -0,0 +1,157 @@
+#lang racket
+
+#|
+Implementation adapted from:
+http://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
+|#
+
+(require "csv-to-vector.rkt")
+(provide (all-defined-out))
+
+(define (data-empty? data)
+  (= (data-length data) 0))
+(define (data-first data)
+  (vector-ref (vector-take data 1) 0))
+(define (data-rest data)
+  (vector-take-right data (- (data-length data) 1)))
+(define (data-length a-data-set)
+  (vector-length a-data-set))
+(define (data-point-length a-data-set)
+  (vector-length a-data-set))
+(define (data-filter predicate a-data-set)
+  (vector-filter predicate a-data-set))
+(define (data-map procedure a-data-set)
+  (vector-map procedure a-data-set))
+(define (data-get-col data col-index)
+  (data-map (lambda (data-point)
+              (data-point-get-col data-point col-index))
+            data))
+(define (data-get-row data row-index)
+  (vector-ref data row-index))
+
+(define (class-equals? class-1 class-2)
+  (= class-1 class-2))
+
+(define (data-point-get-col data-point col-index)
+  (vector-ref data-point col-index))
+
+(define FILE-PATH "data_banknote_authentication.csv")
+(define COLUMN-CONVERTERS (list string->number
+                                string->number
+                                string->number
+                                string->number
+                                (lambda (a-class)
+                                  (inexact->exact
+                                   (string->number a-class)))))
+
+(define data-set (all-rows FILE-PATH
+                           #:column-converters COLUMN-CONVERTERS))
+
+(define FEATURE-COLUMN-INDICES (range (- (data-point-length (data-get-row data-set 0)) 1)))
+(define LABEL-COLUMN-INDEX (- (data-point-length (data-get-row data-set 0)) 1))
+
+
+
+(define (calc-proportion subset class-label label-column-index)
+  (cond [(data-empty? subset) 0]
+        [else (let* ([row-count (data-length subset)]
+                     [class-count
+                      (data-length
+                       (data-filter (lambda (row)
+                                      (class-equals? class-label
+                                                     (data-point-get-col row
+                                                                         label-column-index)))
+                                    subset))]
+                     [prop (/ class-count row-count)])
+                (* prop (- 1.0 prop)))]))
+
+#|
+The procedure gini-index is used to evaluate the quality of a split.
+It is a cost function for a split.
+We want to keep the costs for splits low. (also: greedy)
+There are other ways of calculating the quality of a split, but for now we
+implement gini index.
+|#
+(define (gini-index subsets class-labels label-column-index)
+  (apply +
+         (map (lambda (subset)
+                (apply +
+                       (vector->list ;; possible performance issue
+                        (vector-map
+                         (lambda (class-label)
+                           (calc-proportion subset class-label label-column-index))
+                         class-labels))))
+              subsets)))
+
+(define (split-data data index value)
+  (list (data-filter (lambda (data-point)
+                       (< (data-point-get-col data-point index) value))
+                     data)
+        (data-filter (lambda (data-point)
+                       (>= (data-point-get-col data-point index) value))
+                     data)))
+
+(define (get-best-split data
+                        split-cost-function
+                        feature-column-indices
+                        label-column-index)
+  (define (select-better-split earlier-best-result
+                               split-cost-function
+                               data
+                               split-feature-index
+                               split-value)
+    (let* ([class-labels (data-get-col data label-column-index)]
+           [new-split (split-data data split-feature-index split-value)]
+           [new-split-cost (split-cost-function new-split class-labels label-column-index)])
+      (cond [(< new-split-cost (hash-ref earlier-best-result 'cost))
+             (hash 'index split-feature-index
+                   'value split-value
+                   'subsets new-split
+                   'cost new-split-cost)]
+            [else earlier-best-result])))
+
+  ;; iterates over values of one feature, to find the best split value
+  (define (iter-values split-feature-index remaining-rows current-result)
+    (display "remaining rows: ") (display (data-length remaining-rows)) (newline)
+    (cond [(data-empty? remaining-rows) current-result]
+          [else (iter-values split-feature-index
+                             (data-rest remaining-rows)
+                             (select-better-split current-result
+                                                  split-cost-function
+                                                  data
+                                                  split-feature-index
+                                                  (data-point-get-col (data-first remaining-rows)
+                                                                      split-feature-index)))]))
+
+  ;; iterates over features which might be the split feature
+  (define (iter-features remaining-feature-column-indices
+                         current-result)
+    (display "remaining feature column indices: ")
+    (display remaining-feature-column-indices)
+    (newline)
+    (cond [(empty? remaining-feature-column-indices) current-result]
+          [else (iter-features (rest remaining-feature-column-indices)
+                               (iter-values (first remaining-feature-column-indices)
+                                            data
+                                            current-result))]))
+  ;; starting the whole thing
+  (iter-features feature-column-indices
+                 (hash 'index +inf.0
+                       'value +inf.0
+                       'subsets empty
+                       'cost +inf.0)))
+
+(define TEST-DATA #(#(2.771244718 1.784783929 0)
+                    #(1.728571309 1.169761413 0)
+                    #(3.678319846 2.81281357 0)
+                    #(3.961043357 2.61995032 0)
+                    #(2.999208922 2.209014212 0)
+                    #(7.497545867 3.162953546 1)
+                    #(9.00220326 3.339047188 1)
+                    #(7.444542326 0.476683375 1)
+                    #(10.12493903 3.234550982 1)
+                    #(6.642287351 3.319983761 1)))
+
+
+(time (get-best-split data-set gini-index (list 0 1 2 3) 4))
+#;(time (get-best-split TEST-DATA gini-index (list 0 1) 2))
